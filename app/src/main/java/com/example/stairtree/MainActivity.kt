0464af17fb362f9c2896229dd9695209a5fa5d53
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2021-11-21 22:44:04 +0900

    気圧計測

--- app/src/main/java/com/example/stairtree/MainActivity.kt
+++ app/src/main/java/com/example/stairtree/MainActivity.kt
@@ -1,14 +1,63 @@
 package com.example.stairtree
 
+//import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.stairtree.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+//class MainActivity : AppCompatActivity() {
+//    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(binding.root)
+//        binding.textView.text = "change"
+//    }
+//}
+
+//class SensorActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var pressure: Sensor? = null
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+//        setContentView(binding.root)
         binding.textView.text = "change"
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val millibarsOfPressure = event.values[0]
+        // Do something with this sensor data.
+        Log.i("label", millibarsOfPressure.toString())
+        binding.textView.text = millibarsOfPressure.toString()
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        super.onResume()
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
