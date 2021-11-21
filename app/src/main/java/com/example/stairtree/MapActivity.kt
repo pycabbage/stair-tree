fd6ad73760bef0d1c76f4005032582d7e4ab83ce
鷺空汰 <blancshio@gmail.com>
2021-11-21 13:06:29 +0900

    特定の国の色を変える機能の動作確認 #2

--- app/src/main/java/com/example/stairtree/MapActivity.kt
+++ app/src/main/java/com/example/stairtree/MapActivity.kt
@@ -1,17 +1,17 @@
 package com.example.stairtree
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.stairtree.databinding.ActivityMapBinding
-import androidx.lifecycle.Transformations.map
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.data.geojson.GeoJsonLayer
 
-
-class MapActivity : AppCompatActivity(), OnMapReadyCallback  {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private val binding by lazy { ActivityMapBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +20,19 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback  {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment!!.getMapAsync(this)
+
     }
+
     override fun onMapReady(googleMap: GoogleMap) {
         googleMap.addMarker(
             MarkerOptions()
                 .position(LatLng(0.0, 0.0))
                 .title("Marker")
         )
+        val layer = GeoJsonLayer(googleMap, R.raw.sample, this)
+        layer.addLayerToMap()
+        val PolygonStyle = layer.defaultPolygonStyle
+        PolygonStyle.fillColor = Color.argb(255, 255, 0, 0);
     }
 }
 
