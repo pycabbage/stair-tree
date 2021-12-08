5d2612aad102c7d08c606d68a788f13b6e7608d2
鷺空汰 <blancshio@gmail.com>
2021-12-09 00:17:59 +0900

    geoJsonをクリックすることにより発生するイベントの追加

--- app/src/main/java/com/example/stairtree/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/stairtree/ui/map/MapFragment.kt
@@ -1,9 +1,12 @@
 package com.example.stairtree.ui.map
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.stairtree.R
@@ -45,7 +48,21 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
 
         val country = getCountryGeoJson("usa")
-        fillCountry(googleMap, country)
+        val greenLand = getCountryGeoJson("grl")
+        fillCountry(googleMap,country)
+        fillCountry(googleMap, greenLand).setOnFeatureClickListener { feature ->
+                // BuilderからAlertDialogを作成
+                val dialog = AlertDialog.Builder(requireContext())
+                    .setTitle("グリーンランドについて") // タイトル
+                    .setMessage("グリーンランドは、\n" +
+                            "2100年まで海面が60cm上昇といわれています。") // メッセージ
+                    .setPositiveButton("ok") { dialog, which -> // OK
+                        Toast.makeText(context, "OKがタップされた", Toast.LENGTH_SHORT).show()
+                    }
+                    .create()
+                // AlertDialogを表示
+                dialog.show()
+        }
         setTyphoon(googleMap)
     }
 
@@ -53,9 +70,10 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         return resources.getIdentifier(country, "raw", activity?.packageName)
     }
 
-    fun fillCountry(map: GoogleMap, country: Int) {
+    fun fillCountry(map: GoogleMap, country: Int):GeoJsonLayer {
         val layer = GeoJsonLayer(map, country, context)
         layer.addLayerToMap()
+        return layer
     }
     fun setTyphoon(map:GoogleMap){
         val newarkLatLng = LatLng(35.987261, 138.927622)
