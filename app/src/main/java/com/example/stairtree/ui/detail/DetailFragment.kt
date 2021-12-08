86ac6e16574d6c4c4f626c70fcf56b043f9eee82
鷺空汰 <blancshio@gmail.com>
2021-12-08 22:36:28 +0900

    グラフが動いたやつ

--- app/src/main/java/com/example/stairtree/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/stairtree/ui/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.stairtree.ui.detail
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.stairtree.databinding.FragmentDetailBinding
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class DetailFragment : Fragment() {
 
@@ -19,7 +24,39 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        val lineChart = binding.chart
+        val x = listOf<Float>(1f, 2f, 3f, 5f, 8f, 13f, 21f, 34f)//X軸データ
+        val y = x.map{it*it}//Y軸データ（X軸の2乗）
+        var entryList = mutableListOf<Entry>()//1本目の線
+        for(i in x.indices){
+            entryList.add(
+                Entry(x[i], y[i])
+            )
+        }
+        val lineDataSets = mutableListOf<ILineDataSet>()
+        //②DataSetにデータ格納
+        val lineDataSet = LineDataSet(entryList, "square")
+        //③DataSetにフォーマット指定(3章で詳説)
+        lineDataSet.color = Color.BLUE
+
+        //リストに格納
+        lineDataSets.add(lineDataSet)
+
+        val lineData = LineData(lineDataSets)
+        //⑤LineChartにLineData格納
+
+        lineChart.data = lineData
+        //⑥Chartのフォーマット指定(3章で詳説)
+        //X軸の設定
+        lineChart.xAxis.apply {
+            isEnabled = true
+            textColor = Color.BLACK
+        }
+        //⑦linechart更新
+        lineChart.invalidate()
         return binding.root
+
     }
 
     override fun onDestroyView() {
