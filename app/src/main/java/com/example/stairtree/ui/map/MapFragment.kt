1f97b0f6579af4f9e2b50c54576e61d99d4d5a43
鷺空汰 <blancshio@gmail.com>
2021-12-10 23:33:03 +0900

    エラーの修正　複数の国でタップイベントを実装できるように

--- app/src/main/java/com/example/stairtree/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/stairtree/ui/map/MapFragment.kt
@@ -2,8 +2,6 @@ package com.example.stairtree.ui.map
 
 import android.app.AlertDialog
 import android.graphics.Color
-import android.graphics.Color.RED
-import android.graphics.ColorSpace
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.Log
@@ -21,6 +19,11 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.GroundOverlayOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.collections.GroundOverlayManager
+import com.google.maps.android.collections.MarkerManager
+import com.google.maps.android.collections.PolygonManager
+import com.google.maps.android.collections.PolylineManager
+import com.google.maps.android.data.geojson.GeoJsonFeature
 import com.google.maps.android.data.geojson.GeoJsonLayer
 
 class MapFragment : Fragment(), OnMapReadyCallback {
@@ -35,10 +38,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
 
-        binding.yabasa.max = 100
-        binding.yabasa.min = 0
-        binding.yabasa.progress = 30
-        binding.yabasa.progressDrawable.setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
         return binding.root
     }
 
@@ -54,33 +53,26 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val country = getCountryGeoJson("usa")
-        val greenLand = getCountryGeoJson("grl")
-        fillCountry(googleMap, country)
-        fillCountry(googleMap, greenLand).setOnFeatureClickListener { feature ->
-            // BuilderからAlertDialogを作成
-            val dialog = AlertDialog.Builder(requireContext())
-                .setTitle("グリーンランドについて") // タイトル
-                .setMessage(
-                    "グリーンランドは、\n" +
-                            "2100年まで海面が60cm上昇といわれています。"
-                ) // メッセージ
-                .setPositiveButton("ok") { dialog, which -> // OK
-                    Toast.makeText(context, "OKがタップされた", Toast.LENGTH_SHORT).show()
-                }
-                .create()
-            // AlertDialogを表示
-            dialog.show()
-        }
-        setTyphoon(googleMap)
+
+        level1(googleMap)
+
     }
 
     fun getCountryGeoJson(country: String): Int {
         return resources.getIdentifier(country, "raw", activity?.packageName)
     }
 
-    fun fillCountry(map: GoogleMap, country: Int): GeoJsonLayer {
-        val layer = GeoJsonLayer(map, country, context)
+    fun fillCountry(map: GoogleMap, country: Int,markerManager:MarkerManager,groundOverlayManager:GroundOverlayManager,polygonManager:PolygonManager,polylineManager:PolylineManager): GeoJsonLayer {
+        val layer =  GeoJsonLayer(
+            map,
+            country,
+            context,
+            markerManager,
+            polygonManager,
+            polylineManager,
+            groundOverlayManager
+        )
+
         layer.addLayerToMap()
         return layer
     }
@@ -92,6 +84,50 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             .position(newarkLatLng, 860000f, 650000f)
         map.addGroundOverlay(newarkMap)
 
+    }
+
+    fun level1(googleMap: GoogleMap) {
+        binding.yabasa.max = 100
+        binding.yabasa.min = 0
+        binding.yabasa.progress = 30
+        binding.yabasa.progressDrawable.setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN)
+        // GeoJSON polygon
+        val markerManager = MarkerManager(googleMap)
+        val groundOverlayManager = GroundOverlayManager(googleMap!!)
+        val polygonManager = PolygonManager(googleMap)
+        val polylineManager = PolylineManager(googleMap)
+
+        val usa = getCountryGeoJson("usa")
+        val greenLand = getCountryGeoJson("grl")
+
+        fillCountry(googleMap, greenLand,markerManager,groundOverlayManager,polygonManager,polylineManager).setOnFeatureClickListener {
+            // BuilderからAlertDialogを作成
+            val dialog = AlertDialog.Builder(requireContext())
+                .setTitle("グリーンランドについて") // タイトル
+                .setMessage(
+                    "グリーンランドは、\n" +
+                            "2100年までに海面が60cm上昇するといわれています。"
+                ) // メッセージ
+                .setPositiveButton("ok") { _, _ -> // OK
+                    Toast.makeText(context, "OKがタップされた", Toast.LENGTH_SHORT).show()
+                }
+                .create()
+            // AlertDialogを表示
+            dialog.show()
+        }
+        fillCountry(googleMap, usa,markerManager,groundOverlayManager,polygonManager,polylineManager).setOnFeatureClickListener {
+            val dialog = AlertDialog.Builder(requireContext())
+                .setTitle("アメリカで発生した地球温暖化の事例")
+                .setMessage("アメリカのフロリダ半島に巨大ハリケーンカトリーナが上陸、その後ルイジアナ州に再上陸、大きな被害をもたらしました。")
+                .setPositiveButton("ok") { _, _ -> // OK
+                    Toast.makeText(context, "OKがタップされた", Toast.LENGTH_SHORT).show()
+                }
+                .create()
+            // AlertDialogを表示
+            dialog.show()
+
+        }
+        setTyphoon(googleMap)
     }
 
 }
