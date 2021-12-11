ac6a7ed692b9fefcb4eedfe1e1779027e9633f7f
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2021-12-12 04:40:05 +0900

    グラフ途中

--- app/src/main/java/com/example/stairtree/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/stairtree/ui/detail/DetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.stairtree.ui.detail
 
 import android.graphics.Color
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,9 +16,24 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.utils.ViewPortHandler
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.time.Duration.Companion.days
+
+//class MyXAxisValueFormatter: IndexAxisValueFormatter {
+//    override fun getXValue(dateInMillisecons: String, index: Int, viewPortHandler: ViewPortHandler): String? {
+//        try {
+//            val sdf: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
+//            return sdf.format(Date(dateInMillisecons.toLong()))
+//        } catch (e: Exception) {
+//            return dateInMillisecons
+//        }
+//    }
+//}
 
 class DetailFragment : Fragment() {
 
@@ -28,61 +44,76 @@ class DetailFragment : Fragment() {
     private lateinit var db: AppDatabase
     private lateinit var dailyDatabase: DailyDao
 
+    private fun strDateToFloat(date: String): Float {
+        var df: Float = 0f
+        df += date.split("-")[0].toFloat() * 10000
+        df += date.split("-")[1].toFloat() * 100
+        df += date.split("-")[2].toFloat()
+        return df
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
-
+        val dataE = false  // true: elevator  false: stair
         coroutineScope.launch {
+            val df = SimpleDateFormat("yyyy-MM-dd")
             db = AppDatabase.create(requireContext())
             dailyDatabase = db.daily()
-            for (i in dailyDatabase.selectAll()) {
-                Log.i("aaa", i.toString())
+            var dbdata: MutableMap<String, Double> = mutableMapOf()
+            dailyDatabase.selectAll().forEach {
+                val _data = if (dataE) it.elevator else it.stair
+                if (dbdata.containsKey(it.date)) {
+                    dbdata[it.date] = _data + dbdata[it.date]!!
+                } else {
+                    dbdata[it.date] = _data
+                }
             }
+            val lineChart = binding.chart
+            val x = dbdata.keys.map { strDateToFloat(it) }
+            val y = dbdata.entries.map { it.value.toFloat() }
+            Log.i("x", x.toString())
+            Log.i("y", y.toString())
+            var entryList = mutableListOf<Entry>()  // 1本目の線
+            for (i in x.indices) {
+                entryList.add(
+                    Entry(x[i], y[i])
+                )
+            }
+            val lineDataSets = mutableListOf<ILineDataSet>()
+            //②DataSetにデータ格納
+            val lineDataSet = LineDataSet(entryList, "square")
+            //③DataSetにフォーマット指定(3章で詳説)
+            lineDataSet.color = Color.CYAN
+            lineChart.legend.textColor = Color.CYAN
+
+            //リストに格納
+            lineDataSets.add(lineDataSet)
+            val lineData = LineData(lineDataSets)
+
+            //⑤LineChartにLineData格納
+            lineChart.data = lineData
+
+            //⑥Chartのフォーマット指定(3章で詳説)
+            //X軸の設定
+            lineChart.xAxis.apply {
+                isEnabled = true
+                textColor = Color.GRAY
+            }
+
+            lineChart.axisLeft.apply {
+                textColor = Color.GRAY
+            }
+
+            lineChart.axisRight.apply {
+                textColor = Color.GRAY
+            }
+
+            //⑦linechart更新
+            lineChart.invalidate()
         }
-
-        val lineChart = binding.chart
-        val x = listOf<Float>(1f, 2f, 3f, 5f, 8f, 13f, 21f, 34f)//X軸データ
-        val y = x.map { it * it }//Y軸データ（X軸の2乗）
-        var entryList = mutableListOf<Entry>()//1本目の線
-        for (i in x.indices) {
-            entryList.add(
-                Entry(x[i], y[i])
-            )
-        }
-        val lineDataSets = mutableListOf<ILineDataSet>()
-        //②DataSetにデータ格納
-        val lineDataSet = LineDataSet(entryList, "square")
-        //③DataSetにフォーマット指定(3章で詳説)
-        lineDataSet.color = Color.CYAN
-
-        lineChart.legend.textColor = Color.CYAN
-
-        //リストに格納
-        lineDataSets.add(lineDataSet)
-
-        val lineData = LineData(lineDataSets)
-        //⑤LineChartにLineData格納
-
-        lineChart.data = lineData
-        //⑥Chartのフォーマット指定(3章で詳説)
-        //X軸の設定
-        lineChart.xAxis.apply {
-            isEnabled = true
-            textColor = Color.GRAY
-        }
-
-        lineChart.axisLeft.apply {
-            textColor = Color.GRAY
-        }
-
-        lineChart.axisRight.apply {
-            textColor = Color.GRAY
-        }
-
-        //⑦linechart更新
-        lineChart.invalidate()
         return binding.root
     }
 
