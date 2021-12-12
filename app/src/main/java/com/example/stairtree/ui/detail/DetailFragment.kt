6448091a7d5950404947e7f893847e4aff185c87
鷺空汰 <blancshio@gmail.com>
2021-12-13 02:09:39 +0900

    なんかグラフのやつできた

--- app/src/main/java/com/example/stairtree/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/stairtree/ui/detail/DetailFragment.kt
@@ -2,6 +2,7 @@ package com.example.stairtree.ui.detail
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.fragment.app.viewModels
 import com.example.stairtree.databinding.FragmentDetailBinding
 import com.example.stairtree.db.AppDatabase
 import com.example.stairtree.db.daily.DailyDao
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -18,6 +20,8 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
+
 
 class DetailFragment : Fragment() {
 
@@ -31,39 +35,64 @@ class DetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View{
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         val lineChart = binding.chart
         val toggle = binding.toggleButton
         toggle.setOnCheckedChangeListener { _, isChecked ->
             coroutineScope.launch {
+                val date = Date()
+                val calendar = Calendar.getInstance()
+                var month = mutableListOf<String>()
+                calendar.time = date
+                for (i in 0..30) {
+                    month.add(calendar.get(Calendar.YEAR).toString() + "-" + (calendar.get(Calendar.MONTH)+1).toString()+ "-" + calendar.get(Calendar.DATE).toString())
+                    Log.i("month",calendar.time.toString())
+                    calendar.add(Calendar.DATE, -1)
+                }
+                month.reverse()
+                Log.i("month", month.toString())
+
                 db = AppDatabase.create(requireContext())
                 dailyDatabase = db.daily()
                 val dbData: MutableMap<String, Double> = mutableMapOf()
+
                 dailyDatabase.selectAll().forEach {
                     val data = if (isChecked) it.stair else it.elevator
                     if (dbData.containsKey(it.date)) {
                         dbData[it.date] = data + dbData[it.date]!!
+//                        Log.i("it.date",it.date)
                     } else {
                         dbData[it.date] = data
                     }
                 }
-                val x = dbData.keys.mapIndexed { index, _ ->
-                    (index + 1).toFloat()
+                val x: List<Float> = (0..30).toList().map { it.toFloat() }
+                val y = (0..30).toList().map {
+                    Log.i("month[it]",month[it])
+                    if (dbData.containsKey(month[it])) {
+                         dbData[month[it]]!!.toFloat()
+                    }else {
+                        0F
+                    }
                 }
-                val y = dbData.entries.map { it.value.toFloat() }
                 val entryList: MutableList<Entry> = mutableListOf()  // 1本目の線
                 for (i in x.indices) {
                     entryList.add(Entry(x[i], y[i]))
                 }
 
                 val xAxisFormatter = object : ValueFormatter() {
-                    var count = 0
-                    override fun getFormattedValue(value: Float): String {
-                        // value には 0, 1, 2... という index が入ってくるので
-                        // index からタイムスタンプを取得する
-                        if (count == dbData.keys.size) count = 0
-                        return (dbData.keys.map { it }[count++])
+//                    var count = 0
+//                    override fun getFormattedValue(value: Float): String {
+//                        // value には 0, 1, 2... という index が入ってくるので
+//                        // index からタイムスタンプを取得する
+//                        if (count == dbData.keys.size) count = 0
+//                        return (month[count])
+//                    }
+
+                    override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+                        Log.i("jfsdkljfl", value.toString())
+
+                        return month[value.toInt()].substring(5)
                     }
                 }
 
