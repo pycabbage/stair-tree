b5c7d1e5df575caf21eee40a5b6dd38d475d2a87
tknk0369 <tknk0369@users.noreply.github.com>
2021-12-13 21:43:45 +0900

    コード整形

---
+++ app/src/main/java/com/example/stairtree/ui/map/detail/MapDetailObject.kt
@@ -0,0 +1,55 @@
+package com.example.stairtree.ui.map.detail
+
+object MapDetailObject {
+    val level1Message = listOf(
+        MapDetailEntity(
+            country = "アメリカ",
+            title = "アメリカで発生した地球温暖化の事例",
+            message = "アメリカのフロリダ半島に巨大が上陸、その後ルイジアナ州に再上陸、大きな被害をもたらしました。",
+            countryJson = "usa",
+            articleURL = "https://www.bbc.com/japanese/50384396",
+            latitude = -95.7128,
+            longitude = 37.0902
+        ),
+        MapDetailEntity(
+            country = "ツバル",
+            title = "ツバル",
+            message = "",
+            countryJson = "tuv",
+            articleURL = "https://www3.nhk.or.jp/news/html/20211110/k10013341181000.html",
+            latitude = -8.516667,
+            longitude = 179.216667
+        ),
+        MapDetailEntity(
+            country = "バングラデシュ",
+            title = "バングラデシュ",
+            message = "地球温暖化によるサイクロンによって、多くの死者が出ています",
+            countryJson = "bgd",
+            articleURL = "https://www.unicef.or.jp/news/2019/0057.html",
+            latitude = 22.7,
+            longitude = 90.35,
+        )
+    )
+    val level1size = level1Message.size
+    val level2Message = listOf(
+        MapDetailEntity(
+            country = "グリーンランド",
+            title = "グリーンランドについて",
+            message = " グリーンランドは、2100年までに海面が60cm上昇するといわれています。",
+            countryJson = "grl",
+            articleURL = "https://style.nikkei.com/article/DGXMZO64709280X01C20A0000000/",
+            latitude = 71.7069,
+            longitude = -42.6043
+        ),
+        MapDetailEntity(
+            country = "日本",
+            title = "日本について",
+            message = "日本は100年後スーパー台風ってのがめっちゃ増えます",
+            countryJson = "jpn",
+            articleURL = "https://www.nikkei.com/article/DGXNASDG29034_Z20C12A5CR8000/",
+            latitude = 24.0,
+            longitude = 153.0
+        )
+    )
+    val level2size = level2Message.size
+}
