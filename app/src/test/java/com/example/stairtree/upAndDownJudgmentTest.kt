c6a9210870130b86fe4767cf93b44a688ae3e39c
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2021-12-14 21:50:43 +0900

    テストの関数名変えた

--- app/src/test/java/com/example/stairtree/upAndDownJudgmentTest.kt
+++ app/src/test/java/com/example/stairtree/upAndDownJudgmentTest.kt
@@ -1,6 +1,6 @@
 package com.example.stairtree
 
-import com.example.stairtree.background.upAndDownJudgment
+import com.example.stairtree.background.UpAndDownJudgment
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -8,13 +8,13 @@ import org.junit.Assert.*
 class upAndDownJudgmentTest {
     @Test
     fun getSlope() {
-        val judge = upAndDownJudgment(2)
+        val judge = UpAndDownJudgment(2)
         judge.push(0.2)
         judge.push(0.4)
         assertEquals(1.0, judge.sloop(), 0.1)
         judge.push(0.8)
         assertEquals(2.0,judge.sloop(),0.1)
-        val judge2 = upAndDownJudgment(3)
+        val judge2 = UpAndDownJudgment(3)
         judge2.push(0.2)
         judge2.push(0.4)
         judge2.push(0.6)
@@ -24,7 +24,7 @@ class upAndDownJudgmentTest {
     }
     @Test
     fun canGetSlope(){
-        val judge = upAndDownJudgment(2)
+        val judge = UpAndDownJudgment(2)
         judge.push(0.2)
         assert(!judge.possibleToJudge())
         judge.push(0.4)
