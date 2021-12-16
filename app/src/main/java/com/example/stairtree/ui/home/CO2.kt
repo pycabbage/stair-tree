81e8c743c6d5ce381b21893fb25701bed7a3db66
tknk0369 <tknk0369@users.noreply.github.com>
2021-12-16 22:38:37 +0900

    ホーム画面すごくした

--- app/src/main/java/com/example/stairtree/ui/home/CO2.kt
+++ app/src/main/java/com/example/stairtree/ui/home/CO2.kt
@@ -15,7 +15,8 @@ import kotlin.math.sin
 class CO2 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    var speed = 0.025
+    var number = 2
+    private var speed = 0.025
     private var count = 0.0
     private val co2Image = BitmapFactory.decodeResource(resources, R.drawable.co2)
     private val rect = Rect(0, 0, 0, 0)
@@ -25,27 +26,37 @@ class CO2 @JvmOverloads constructor(
         val s = (sin(count) * 100).toInt()
         val c = (cos(count) * 100).toInt()
         val sc = (sin(count) * cos(count) * 100).toInt()
-        rect.set(
-            width * 1 / 4 - 50 + s,
-            height * 4 / 7 + c,
-            width * 1 / 4 + 50 + s,
-            height * 4 / 7 + 100 + c
-        )
-        canvas?.drawBitmap(co2Image, null, rect, paint)
-        rect.set(
-            width * 2 / 4 - 50 + c,
-            height * 2 / 6 + sc,
-            width * 2 / 4 + 50 + c,
-            height * 2 / 6 + 100 + sc
-        )
-        canvas?.drawBitmap(co2Image, null, rect, paint)
-        rect.set(
-            width * 3 / 4 - 50 + sc,
-            height * 3 / 6 + s,
-            width * 3 / 4 + 50 + sc,
-            height * 3 / 6 + 100 + s
-        )
-        canvas?.drawBitmap(co2Image, null, rect, paint)
+
+        if (number == 0 || number == 1 || number == 2) {
+            rect.set(
+                width * 2 / 4 - 50 + c,
+                height * 2 / 6 + sc,
+                width * 2 / 4 + 50 + c,
+                height * 2 / 6 + 100 + sc
+            )
+            canvas?.drawBitmap(co2Image, null, rect, paint)
+        }
+        
+        if (number == 1 || number == 2) {
+            rect.set(
+                width * 1 / 4 - 50 + s,
+                height * 4 / 7 + c,
+                width * 1 / 4 + 50 + s,
+                height * 4 / 7 + 100 + c
+            )
+            canvas?.drawBitmap(co2Image, null, rect, paint)
+        }
+
+        if (number == 2) {
+            rect.set(
+                width * 3 / 4 - 50 + sc,
+                height * 3 / 6 + s,
+                width * 3 / 4 + 50 + sc,
+                height * 3 / 6 + 100 + s
+            )
+            canvas?.drawBitmap(co2Image, null, rect, paint)
+        }
+
         count += speed
         invalidate()
     }
--- app/src/main/java/com/example/stairtree/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/stairtree/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.stairtree.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -39,12 +40,23 @@ class HomeFragment : Fragment() {
         }
 
         model.selectSum.observe(viewLifecycleOwner) {
-            binding.usage.text = if (it.elevator > it.stair) {
-                binding.treeimage.setImageResource(R.drawable.kareta_ki)
-                "個人:木%,.2f本分の二酸化炭素排出...".format(it.elevator - it.stair)
-            } else {
-                binding.treeimage.setImageResource(R.drawable.ki)
-                "個人:木%,.2f本分の二酸化炭素削減!".format(it.stair - it.elevator)
+            binding.usage.text = when {
+                it.elevator > it.stair * 1000 -> {
+                    binding.treeimage.setImageResource(R.drawable.kareta_ki2)
+                    binding.CO2.number = 2
+                    "個人:木%,.2f本分の二酸化炭素排出...".format(it.elevator - it.stair)
+                }
+                it.elevator > it.stair -> {
+                    //binding.treeimage.setImageResource(R.drawable.sukoshi_kareta_ki)
+                    binding.treeimage.setImageResource(R.drawable.kareta_ki2)
+                    binding.CO2.number = 1
+                    "個人:木%,.2f本分の二酸化炭素排出...".format(it.elevator - it.stair)
+                }
+                else -> {
+                    binding.treeimage.setImageResource(R.drawable.ki)
+                    binding.CO2.number = 0
+                    "個人:木%,.2f本分の二酸化炭素削減!".format(it.stair - it.elevator)
+                }
             }
         }
 
